aa104a7c5a81bff06624ee98b6d44cf01027cd47
AugustoRajao <augustocedro9@gmail.com>
2024-10-14 16:48:14 -0300

    ClientDaoTest implemented

--- src/test/ClientDaoJDBCTest.java
+++ src/test/ClientDaoJDBCTest.java
@@ -10,19 +10,68 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-public class ClientDaoJDBCTest {
+public class ClientDaoJDBCTest  {
     private static ClientDao clientDao = DaoFactory.createClientDao();
 
     @Test
     public void insert(){
-//        List<Client> clientList = CSVReader.readClients("clients.csv");
-//        for(Client c : clientList){
-//            clientDao.insert(c);
-//        }
-//        assertEquals(clientList.size(),clientDao.findAll().size());
+        Client c = clientDao.findById(1);
+        c.setEmail("test@gmail.com");
+        clientDao.insert(c);
+        assertEquals("test@gmail.com",clientDao.findById(c.getId()).getEmail());
+        clientDao.deleteByEmail(c.getEmail());
     }
     @Test
     public void findByEmail(){
-        assertEquals("ana.silva@example.com",clientDao.findByEmail("ana.silva@example.com").getEmail());
+        assertEquals("ana.silva.souza@example.com",clientDao.findByEmail("ana.silva.souza@example.com").getEmail());
     }
+
+    @Test
+    public void findById(){
+        Client c = clientDao.findById(1);
+        assertEquals("ana.silva.souza@example.com",c.getEmail());
+    }
+
+    @Test
+    public void findByName(){
+        List<Client> list = clientDao.findByName("Lucas Almeida");
+        assertEquals("Lucas Almeida",list.get(0).getName());
+    }
+
+    @Test
+    public void deleteById(){
+        Client c = clientDao.findById(1);
+        c.setEmail("test@gmail.com");
+        clientDao.insert(c);
+        clientDao.deleteByEmail("test@gmail.com");
+        assertEquals(null,clientDao.findByEmail("test@gmail.com"));
+    }
+
+    @Test
+    public void deleteByEmail(){
+        Client c = clientDao.findById(1);
+        c.setEmail("test@gmail.com");
+        clientDao.insert(c);
+        clientDao.deleteByEmail("test@gmail.com");
+        assertEquals(null,clientDao.findByEmail("test@gmail.com"));
+    }
+
+    @Test
+    public void update(){
+        Client c = clientDao.findById(1);
+        c.setEmail("test@gmail.com");
+        clientDao.insert(c);
+        c.setName("Teste");
+        clientDao.update(c);
+        assertEquals("Teste",clientDao.findById(c.getId()).getName());
+        clientDao.deleteByEmail("test@gmail.com");
+    }
+
+    @Test
+    public void findAll(){
+        List<Client> list = clientDao.findAll();
+        assertEquals(10,list.size());
+    }
+
+
 }
